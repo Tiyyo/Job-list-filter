2b8b27813ca7908d28c24c06bef43e03f81284d1
Steeve Matou <steeve.matou@gmail.com>
2022-11-09 09:27:22 +0100

    get back at it

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ let allTags = [];
 let jobs;
 let activeFilters = [];
 let activeTags = [];
+let condition;
+
+async function getJobs() {
+  await fetch("data.json")
+    .then((res) => res.json())
+    .then((data) => (jobs = data))
+    .then(() => UI.displayJobs(jobs));
+}
 
 class UI {
   static displayJobs(datas) {
@@ -24,9 +32,9 @@ class UI {
           ];
           let tags = newArr;
           if (activeFilters.length >= 0) {
-            console.log("test");
-            console.log(activeFilters.toString());
-            return tags.includes("JavaScript", "Frontend");
+            // console.log(activeFilters[0]);
+            // return tags.includes("Junior");
+            return true;
           } else {
             return true;
           }
@@ -43,7 +51,7 @@ class UI {
               return `<div class="tags" id="tag--job">${tag}</div>`;
             })
             .join("");
-          return `<div class="offer" data-id="${data.id}">
+          return `<div class="offer" data-id="${data.id}" data-featured="${data.featured}">
                         <div class="offer__logo">
                             <img src="${data.logo}" alt="logo of the company" />
                         </div>
@@ -53,11 +61,11 @@ class UI {
                             <h5 class="offer__company__featured" data-featured="${data.featured}">FEATURED</h5>
                         </div>
                         <h4 class="offer__position">${data.position}</h4>
-                        <div class="offer__infos">
-                            <span class="offer__infos__time">${data.postedAt}</span>.
-                            <span class="offer__infos__type--contract">${data.contract}</span>.
-                            <span class="offer__infos__location">${data.location}</span>
-                        </div>
+                        <ul class="offer__infos">
+                            <li class="offer__infos__time">${data.postedAt}</li> 
+                            <li class="offer__infos__type--contract">${data.contract}</li> 
+                            <li class="offer__infos__location">${data.location}</li>
+                        </ul>
                         <div class="offer__filters">
                             ${tags}
                         </div>
@@ -78,24 +86,29 @@ class UI {
 }
 
 window.addEventListener("load", () => {
-  fetch("data.json")
-    .then((res) => res.json())
-    .then((data) => (jobs = data))
-    .then(() => {
-      UI.displayJobs(jobs);
-      let allTags = document.querySelectorAll("#tag--job");
-      allTags.forEach((tag) => {
-        tag.addEventListener("click", (e) => {
-          let content = e.target.textContent;
-          activeFilters.push(content);
-          activeFilters = [...new Set(activeFilters)];
-          UI.displayFilters(activeFilters);
-          console.log(activeFilters.toString());
-          let activeTags = document.querySelectorAll(".active--tags");
-          activeTags.forEach((tag) => {
-            tag.addEventListener("click", (e) => {});
-          });
-        });
-      });
-    });
+  getJobs();
 });
+
+// window.addEventListener("load", () => {
+//   fetch("data.json")
+//     .then((res) => res.json())
+//     .then((data) => (jobs = data))
+//     .then(() => {
+//       UI.displayJobs(jobs);
+//       let allTags = document.querySelectorAll("#tag--job");
+//       allTags.forEach((tag) => {
+//         tag.addEventListener("click", (e) => {
+//           let content = e.target.textContent;
+//           activeFilters.push(content);
+//           activeFilters = [...new Set(activeFilters)];
+//           UI.displayFilters(activeFilters);
+//           console.log(activeFilters);
+//           let activeTags = document.querySelectorAll(".active--tags");
+//           UI.displayJobs(jobs);
+//           activeTags.forEach((tag) => {
+//             tag.addEventListener("click", (e) => {});
+//           });
+//         });
+//       });
+//     });
+// });
